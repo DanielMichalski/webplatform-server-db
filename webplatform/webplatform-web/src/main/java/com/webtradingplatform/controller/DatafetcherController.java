1492a3693697527a5ee5a10381f73a004187d397
bakeraw2 <michalskidaniel2@gmail.com>
2014-08-29 12:33:50 +0200

    Refactoring
    Refactoring Datafetchera oraz poprawienie propertiesów
    
    Refactoring Datafetchera oraz poprawienie propertiesów

--- webplatform/webplatform-web/src/main/java/com/webtradingplatform/controller/DatafetcherController.java
+++ webplatform/webplatform-web/src/main/java/com/webtradingplatform/controller/DatafetcherController.java
@@ -9,6 +9,7 @@
 //import org.apache.http.message.BasicHeader;
 //import org.apache.http.util.EntityUtils;
 //import org.slf4j.Logger;
+//import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.servlet.ModelAndView;
@@ -18,29 +19,33 @@
 //import java.io.InputStream;
 //import java.io.InputStreamReader;
 //
-//
 ///**
-//* Created by Tomasz Mr Phi Waszczyk on 2014-08-27.
-//*/
-//
+// * Created by Tomasz Mr Phi Waszczyk on 2014-08-27.
+// */
+////TODO przeniesc odpowiedzialnosc do serwisu oraz refactoring kodu.
 //@Controller
-//
 //public class DatafetcherController {
 //
-//    private int counter;
-//    DefaultHttpClient httpClient = new DefaultHttpClient();
 //    @AutowiredLogger
 //    private Logger logger;
 //
-//    @RequestMapping("/datafetcher")
-//    public ModelAndView getCandlesData(){
+//    @Value("${datafetcher.url}")
+//    private String url;
 //
-//        ModelAndView data = new ModelAndView();
+//    @Value("${datafetcher.authHeader}")
+//    private String authHeader;
+//
+//    private int counter;
+//
+//    private DefaultHttpClient httpClient = new DefaultHttpClient();
+//
+//    @RequestMapping("/datafetcher")
+//    public ModelAndView getCandlesData(ModelAndView data) {
 //
 //        while(counter < 1){
 //
-//            HttpUriRequest httpGet = new HttpGet("https://api-fxpractice.oanda.com/v1/candles?instrument=EUR_USD&count=10&candleFormat=midpoint&granularity=M15");
-//            httpGet.setHeader(new BasicHeader("Authorization", "Bearer 861521481e615b0c0af6e2163eb011c5-0844fb72267293a47c3031d50ca527ac"));
+//            HttpUriRequest httpGet = new HttpGet(url);
+//            httpGet.setHeader(new BasicHeader("Authorization", authHeader));
 //            logger.info("Initialization", httpGet.getRequestLine());
 //
 //            try {
@@ -48,24 +53,16 @@
 //                HttpEntity entity = resp.getEntity();
 //
 //                if(resp.getStatusLine().getStatusCode() == 200 && entity != null){
-//
 //                    InputStream stream = entity.getContent();
 //                    String line;
 //                    BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-//                    logger.info("CONNECTED ! ! ! ! ! ! ");
+//                    logger.info("CONNECTED!");
 //                    while((line = br.readLine()) != null){
-//
 //                        logger.info(line);
 //                    }
-//
-//
-//
-//
-//
 //                }else{
 //                    String responseString = EntityUtils.toString(entity, "UTF-8");
 //                    logger.info(responseString);
-//
 //                }
 //            } catch (IOException e) {
 //                e.printStackTrace();
@@ -75,11 +72,7 @@
 //            }
 //        }
 //
-//
-//
 //        data.addObject(counter);
-//
-//
 //        return data;
 //    }
 //}
